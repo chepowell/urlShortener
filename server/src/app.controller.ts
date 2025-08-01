a419c845ebbd607ac23386935b602a69ed38c770
Chris Powell <chepowel@gmail.com>
2025-07-31 19:33:44 -0700

    add /shorten endpoint to generate slug for original URL and updated app.controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { UrlService } from './url/url.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly urlService: UrlService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':slug')
+  handleRedirect(@Param('slug') slug: string, @Res() res: Response) {
+    const originalUrl = this.urlService.getOriginalUrl(slug);
+    if (!originalUrl) {
+      return res.status(404).send('Not found');
+    }
+
+    return res.redirect(originalUrl);
   }
 }
---
+++ server/src/url/url.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import { UrlService } from './url.service';
+
+@Controller('shorten')
+export class UrlController {
+  constructor(private readonly urlService: UrlService) {}
+
+  @Post()
+  async createShortUrl(@Body() body: { originalUrl: string }) {
+    const { originalUrl } = body;
+
+    if (!originalUrl || !originalUrl.startsWith('http')) {
+      throw new BadRequestException('Invalid URL');
+    }
+
+    const slug = await this.urlService.generateSlug(originalUrl);
+    return { slug, shortUrl: `http://localhost:5000/${slug}` };
+  }
+}
